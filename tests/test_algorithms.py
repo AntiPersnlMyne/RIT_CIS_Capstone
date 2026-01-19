d5cf2838ad0d4d92e921f79b6f68cb80c7eaf7b1
g-m <mtifone2022@tutamail.com>
2026-01-19 16:13:43 -0500

    Fixed ANOTHER typo

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -12,13 +12,13 @@ Examples
 --------
 
 # Run SAM
-test/test_algorithms.py -a sam -i data/datacubes/archimedes_cubes/Arch_93r_92v_bgp.npy -t results/arch_test.npz
+tests/test_algorithms.py -a sam -i data/datacubes/archimedes_cubes/Arch_93r_92v_bgp.npy -t results/arch_test.npz
 
 # Run OSP  with batch processing
-test/test_algorithms.py -a osp -i data/datacubes/archimedes_cubes/Arch_93r_92v_bgp.npy -t results/arch_test.npz -O
+tests/test_algorithms.py -a osp -i data/datacubes/archimedes_cubes/Arch_93r_92v_bgp.npy -t results/arch_test.npz -O
 
 # Run GOSP with HIGH chunked processing for faster throughput
-test/test_algorithms.py -a gosp -i data/datacubes/archimedes_cubes/Arch_93r_92v_bgp.npy -t results/arch_test.npz -b high
+tests/test_algorithms.py -a gosp -i data/datacubes/archimedes_cubes/Arch_93r_92v_bgp.npy -t results/arch_test.npz -b high
 """
 
 # Mainstream packages
