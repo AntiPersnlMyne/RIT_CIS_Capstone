Fix col crop in wrong dimension

utils/automation.py
@@ -174,7 +174,6 @@ def import_datacube(
         normalize=normalize,
         cachemax_mb=cachemax,
     )
-    print(f"Initial shape: {datacube.shape}")
 
     # Get shape
     rows, cols, _ = datacube.shape
@@ -197,9 +196,6 @@ def import_datacube(
     # Crop rows by pixel count
     else:
         datacube = datacube[row_start:-row_end, :, :]
-        
-    print(f"Initial shape: {datacube.shape}")
-    
 
     # Crop cols by percent
     if col_start <= 1.0 and col_end <= 1.0:
@@ -207,10 +203,10 @@ def import_datacube(
         col_start = int(col_start * cols)
         col_end = int(col_end * cols)
         # Crop
-        datacube = datacube[col_start:-col_end, :, :]
+        datacube = datacube[:, col_start:-col_end, :]
     # Crop cols by pixel count
     else:
-        datacube = datacube[col_start:-col_end, :, :]
+        datacube = datacube[:, col_start:-col_end, :]
 
     assert isinstance(datacube, np.memmap), "np.NDArray returned instead of np.memmap"
     return datacube, datacube_name