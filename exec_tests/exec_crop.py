3caafe29a2ffe6c7fb23e43ab33a7eeaf3f1b0e9
g-m <mt9485@rit.edu>
2026-03-22 17:19:26 -0400

    Saves TIFF and EPS

--- exec_tests/exec_crop.py
+++ exec_tests/exec_crop.py
@@ -14,50 +14,40 @@ equally created (not eyeballed).
 Set the crop bounds inside the "Parameter(s)" section. I'm too tired
 to add a CLI.
 
-Shows the original image (left) and cropped version (right). When
-the figure is closed, saves cropped verison automatically.
-
-Always saves cropped image as a TIFF.
+Saves two cropped version: TIFF (raster, lossless) and EPS (vector).
 
 -------
 Example
 -------
-
 # Just run the script, no parameters required
 python exec_tests/exec_crop.py
 """
 
 # Mainstream packages
-from pathlib import Path
 import matplotlib.pyplot as plt
 from tifffile import imread, imwrite
-from os import makedirs
 
 # ------- Parameter(s) -----------
-uncropped_image = "results/score_maps/102r-98v_nouv/Test6/sam-0.tiff"
-out_dir = "results/figures/102r-98v/Test6"
+
+datacube_name = "102r-98v"
+test_name = "test7"
+
+# Image to be cropped (src)
+uncropped_image = f"results/figures/{datacube_name}/pseudocolors/{test_name}.tiff"
+
+out_base = f"results/figures/{datacube_name}/cropped_pseudocolors/{test_name}"
+out_tiff = f"{out_base}.tiff" # Saves TIFF
+out_eps = f"{out_base}.eps"   # Saves EPS
 
 # Pixels removed off of sides of image
 pixels_off_the_top = 4300
-pixels_off_the_bottom = 3300
-pixels_off_the_left = 500
-pixels_off_the_right = 3500
+pixels_off_the_bottom = 5000
+pixels_off_the_left = 700
+pixels_off_the_right = 3650
 # ------------------------------
 
 
 if __name__ == "__main__":
-    
-    # Save cropped and uncropped image with same name
-    file_name = Path(uncropped_image).parts[-1]
-    
-    # Create output path if doesn't exist
-    makedirs(out_dir, exist_ok=True)
-
-    # Two plots, side-by-side
-    # Left plot: original
-    # Right plot: cropped
-    fig, ax = plt.subplots(nrows=1, ncols=2)
-
     # Read in image
     uncropped_image = imread(uncropped_image)
 
@@ -66,10 +56,9 @@ if __name__ == "__main__":
         pixels_off_the_top:-pixels_off_the_bottom,  # vertical crop
         pixels_off_the_left:-pixels_off_the_right,  # horizontal crop
     ]
-
-    # Append filename and extension to make the file path
-    out_path = Path(out_dir, file_name).with_suffix(".tiff")
     
-    # Crop and save bounds
-    print(f"Saving cropped image to: '{out_path}'")
-    imwrite(out_path, cropped_image)
+    # Save
+    print(f"Saving cropped image(s) to: '{out_base}'")
+    imwrite(out_tiff, cropped_image)    # TIFF
+    plt.imshow(cropped_image / 65535.0) # EPS
+    plt.savefig(out_eps, format="eps")  
